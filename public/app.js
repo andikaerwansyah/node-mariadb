661663d3200897ea367dd476989bb734ecac898b
Andika Erwansyah <andika.erwansyah@gmail.com>
2019-01-05 14:25:08 +0700

    change query selector from class to id

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 const CreateUser = document.querySelector('.CreateUser')
 CreateUser.addEventListener('submit', (e) => {
   e.preventDefault()
-  const username = CreateUser.querySelector('.username').value
-  const password = CreateUser.querySelector('.password').value
+  const username = CreateUser.querySelector('#username').value
+  const password = CreateUser.querySelector('#password').value
   post('/createUser', {username,password})
 })
 
